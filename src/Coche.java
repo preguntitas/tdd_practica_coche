3ed787d7a1d58d0dede594d8d423007c377cfaff
Mariano Murcia Ortuño <m.murcia@email.com>
2023-03-25 21:18:03 +0100

    Refactorizado

--- src/Coche.java
+++ src/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerar_mariano_murcia(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void decelerar(int deceleracion) {
+    public void decelerar_mariano_murcia(int deceleracion) {
         velocidad -= deceleracion;
         if (velocidad < 0 ) velocidad =0;
     }
--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -3,28 +3,29 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_al_crear_un_coche_su_velocidad_es_cero() {
+    public void test_al_crear_un_coche_su_velocidad_es_cero_mariano_murcia() {
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0,nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta_mariano_murcia() {
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerar_mariano_murcia(30);
         Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_disminuye() {
+    public void test_al_decelerar_un_coche_su_velocidad_disminuye_mariano_murcia() {
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerar_mariano_murcia(20);
         Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero() {
+    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_mariano_murcia() {
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerar_mariano_murcia(80);
         Assertions.assertEquals(0,nuevoCoche.velocidad);
     }
 }
+

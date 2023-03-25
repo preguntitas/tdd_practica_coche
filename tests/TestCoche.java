45b21ed3a7bb70c0c292404cb55fc015d4e191c9
Mariano Murcia Ortuño <m.murcia@email.com>
2023-03-25 20:52:39 +0100

    Test velocidad aumenta

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -3,8 +3,14 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_al_crear_coche_su_velocidad_es_cero() {
+    public void test_al_crear_un_coche_su_velocidad_es_cero() {
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0,nuevoCoche.velocidad);
+    }
+    @Test
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.acelerar(30);
+        Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
 }
